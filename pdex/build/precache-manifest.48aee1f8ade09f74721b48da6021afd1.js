a0b0785905eb9beeae3b6031cb5fd3b71a23520f
EGarciaSEC <egarciasec@gmail.com>
2018-11-27 15:08:01 -0300

    Added Types

---
+++ pdex/build/precache-manifest.48aee1f8ade09f74721b48da6021afd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "529ae48d98a691ed2370",
+    "url": "/pdex/static/css/main.85b7bf43.chunk.css"
+  },
+  {
+    "revision": "529ae48d98a691ed2370",
+    "url": "/pdex/static/js/main.529ae48d.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/pdex/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "d1b161a9793c672b0464",
+    "url": "/pdex/static/js/runtime~main.d1b161a9.js"
+  },
+  {
+    "revision": "88aef6833d3d2bfb772a2fde8ad63f1f",
+    "url": "/pdex/index.html"
+  }
+];
